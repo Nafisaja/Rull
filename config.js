8f6f6c0fb7610612f3cc80896c2538fd456c3610
Nafisaja <105576471+Nafisaja@users.noreply.github.com>
2022-08-26 19:46:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 //=============『 MAIN 』================== //
-global.owner = ['6285746030186', '6285607831410'] // Ganti Jadi Nomormu
+global.owner = ['628888333057', '628888333057'] // Ganti Jadi Nomormu
 global.mods = ['0'] 
 global.prems = [''] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'Nama Owner'
-global.numberowner = '6285746030186' // Ganti Jadi Nomor mu
+global.nameowner = 'Addy'
+global.numberowner = '628888333057' // Ganti Jadi Nomor mu
 global.instagram = '-' // Ig Kamu
 global.github = 'https://github.com/ZennRull' // Github kamu (biarin aja kalau gaada)
 global.dana = '-' //Dana pulsa gopay
@@ -13,7 +13,7 @@ global.pulsa = '-'
 global.gopay = '-'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'Nama Bot' // Nama Bot
+global.namebot = 'Evox' // Nama Bot
 global.gc = 'https://t.me/pixelbotwhatsapp'
 global.web = 'https://t.me/pixelbotwhatsapp' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 15K\n2 Minggu 10K\n1 Minggu 5K'
